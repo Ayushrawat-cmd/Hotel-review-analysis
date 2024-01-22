fe8c92bfa5176d758ca8d465fca70226c42ffae8
ayush rawat <ayushrawat324@gmail.com>
2024-01-22 17:54:15 +0530

    add comments

--- Day 1/p4.cpp
+++ Day 1/p4.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-const regex pattern ("((http(s)?)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)");
+const regex pattern ("((http(s)?)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)"); // regex pattern for valid url
 bool valid_url(string s){
     for(auto i : s){
-        if(i==' ')
+        if(i==' ') // space then invalid
             return false;
     }
-    if(regex_match(s, pattern)){
+    if(regex_match(s, pattern)){// if pattern match then true
         return true;
     }
     return false;
 }
 int main(){
     string s;
-    getline(cin, s);
-    if(valid_url(s))
+    getline(cin, s); // enter line
+    if(valid_url(s)) // check for validity
         cout<< "Valid";
     else
         cout<<"Not valid";
