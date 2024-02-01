c34db2af29b3cbc86e8ff79966fe1e09110bebc8
ayush rawat <ayushrawat324@gmail.com>
2024-02-01 23:34:00 +0530

    day 4

---
+++ Day 4/Member.py
@@ -0,0 +1,45 @@
+from User import User
+from Book import Book
+class Member(User):
+    __reserve_books = None
+    __issued_books = None
+    __issued_books_date = None
+    __max_checkout_books = int
+    def __init__(self, name,address, id  ):
+        super().__init__(name, address, id)
+        self.__issued_books = [Book]
+        self.__reserve_books = [Book]
+        self.__issued_books_date = []
+        self.__max_checkout_books = 5
+
+    def checkOutBook(self, book : Book, date) -> bool:
+        '''Checkout functionality for the user by adding certain validation in it'''
+        for issued_book in range(len(self.__issued_books)):
+            if self.__issued_books[issued_book].getId() == book.getId():
+                self.__issued_books_date[issued_book] = date
+                return True
+        if(len(self.__issued_books) == self.__max_checkout_books ): #maximum books got issued
+            return False
+        self.__issued_books.append(book)
+        self.__issued_books_date.append(date)
+        return True
+
+    def reserveBook(self, book : Book) -> None:
+        '''Reserve the book by the user'''
+        self.__reserve_books.append(book)
+    
+    def returnBook(self, book :Book)->bool:
+        '''Return book to the system'''
+        for issued_book in range(len(self.__issued_books)):
+            if self.__issued_books[issued_book].getId() == book.getId():
+                self.__issued_books.pop(issued_book)
+                self.__issued_books_date.pop(issued_book)
+                return True
+        return False
+     
+    
+
+        
+    
+
+
