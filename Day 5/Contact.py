a2acad8ab46efc572dfbc033751b4b2fa6732913
Ayush Rawat <79196598+Ayushrawat-cmd@users.noreply.github.com>
2024-02-12 18:54:28 +0530

    Delete Day 5 directory

--- Day 5/Contact.py
+++
@@ -1,8 +0,0 @@
-from Person import Person
-class Contact(Person):
-    '''Contact is the person'''
-    __id= None
-    def __init__(self, id) -> None:
-        self.__id = id
-    def getID(self):
-        return self.__id
--- Day 5/Text.py
+++
@@ -1,7 +0,0 @@
-class Text:
-    __content = None
-    def __init__(self, content ) -> None:
-        self.__content = content
-    def getContent(self):
-        '''Get the content'''
-        return self.__content
